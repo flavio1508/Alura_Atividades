a92bd13098f0a2102f3fd884292269ea88c3d558
Flávio Augusto <flavioaugusto1508@gmail.com>
2023-01-02 17:31:36 -0400

    first commit

---
+++ src/Programa.java
@@ -0,0 +1,8 @@
+public class Programa {
+    public static void main(String[] args) {
+        double salario = 33000.0;
+        if ( salario > 1900.0 || salario <= 2800.0){
+            System.out.println("Seu impost de renda é de 7.5%");
+        }
+    }
+}
